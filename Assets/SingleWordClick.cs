3cec41d033f331c8c9d6499430286b6b647345ad
panzehong <2917179196@qq.com>
2019-11-29 15:34:08 +0800

    1

---
+++ Assets/SingleWordClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SingleWordClick : MonoBehaviour,IPointerDownHandler {
+
+    [SerializeField] RectTransform rectTransform;
+    Rect rect;
+
+   
+
+    // Use this for initialization
+    void Start () {
+        rect = rectTransform.rect;
+
+    }
+    private void Update()
+    {
+        Debug.Log(rect.Contains(Input.mousePosition));
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(rect.Contains(Input.mousePosition));
+    }
+}
---
+++ Assets/process_thread_Coroutine/Coroutine.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class Coroutine : MonoBehaviour {
+
+
+    void Start()
+    {
+        StartCoroutine(tt());//开启协程
+        for (int i = 0; i < 200; i++)   //循环A
+        {
+            Debug.Log("循环A...ing" + i);
+            Thread.Sleep(10);
+        }
+    }
+
+
+    IEnumerator tt()
+    {
+        for (int i = 0; i < 100; i++) //循环B
+        {
+            Debug.Log("循环B...ing" + i);
+        }
+
+        yield return new WaitForSeconds(1); //协程1
+
+        for (int i = 0; i < 100; i++) //循环C
+        {
+            Debug.Log("循环C...ing" + i);
+            yield return null; //协程1
+        }
+    }
+
+    // 更新数据
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+
+    //晚于更新
+    void LateUpdate()
+    {
+        Debug.Log("LateUpdate");
+    }
+
+}
